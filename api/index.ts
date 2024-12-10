0d68a1dcbc0980c7f461b3788037b6147a07eea7
Vitaly Turovsky <vital2580@icloud.com>
2024-12-10 05:52:54 +0300

    it works, pass headers

---
+++ api/index.ts
@@ -0,0 +1,46 @@
+import { IncomingMessage, ServerResponse } from 'http';
+// import fetch from 'node-fetch';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+const pipelineAsync = promisify(pipeline);
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+    const url = new URL(req.url!, `http://${req.headers.host}`).searchParams.get('url');
+
+    if (!url) {
+        res.statusCode = 400;
+        res.end('URL is required');
+        return;
+    }
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                ...Object.fromEntries(Object.entries(req.headers).filter(([key]) => !['host', 'connection', 'accept-encoding', 'origin'].includes(key))) as any,
+            }
+        });
+
+        if (!response.ok) {
+            res.statusCode = response.status;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ error: `Failed to fetch the file: ${response.statusText} (${response.status})` }));
+            return;
+        }
+
+        // Copy the headers from the response to the server response eg for caching
+        for (const [key, value] of response.headers.entries()) {
+            res.setHeader(key, value);
+        }
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        await pipelineAsync(response.body!, res);
+    } catch (error) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ error: `Internal Server Error: ${error.message}` }));
+    }
+}
